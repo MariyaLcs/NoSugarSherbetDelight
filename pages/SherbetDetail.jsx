ecc85367beea3c970432bf6841f8202c9c875c21
Mariya Podosinova <mariya.podosinova@gmail.com>
2024-02-26 09:59:56 +0000

    add details component

--- pages/SherbetDetail.jsx
+++ pages/SherbetDetail.jsx
@@ -4,5 +4,22 @@ import { useParams } from "react-router-dom";
 
 export default function SherbetDetail() {
   const params = useParams();
-  return <h1>HOIHOHO</h1>;
+  const [sherbet, setSherbet] = useState(null);
+
+  useEffect(() => {
+    const url = `/api/sherbet/${params.id}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setSherbet(data.sherbet);
+        console.log(data.sherbet.name);
+        console.log(sherbet);
+      })
+      .catch((err) => console.error("Error ", err));
+  }, [params.id]);
+  return (
+    <div>
+      <h1>Sherbet Details: </h1>
+    </div>
+  );
 }
